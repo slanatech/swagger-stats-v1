bb585298c9c2366f37263b548ba7074193e7db52
sv2 <sv2@slana.tech>
2022-02-19 11:14:58 -0800

    Tests

--- packages/collector/main/validator.js
+++ packages/collector/main/validator.js
@@ -9,6 +9,142 @@ const matcher = require('./matcher');
 const promClient = require('prom-client');
 const monitor = require('./monitor');
 
+// TEMP
+const allM = [
+  {
+    help: 'Total processed spans',
+    name: 'sws_spans_processed_total',
+    type: 'counter',
+    values: [
+      {
+        value: 130,
+        labels: {},
+      },
+    ],
+    aggregator: 'sum',
+  },
+  {
+    help: 'Processed spans per service',
+    name: 'sws_service_spans_processed_total',
+    type: 'counter',
+    values: [
+      {
+        value: 1,
+        labels: {
+          service: 'hapitest',
+        },
+      },
+      {
+        value: 98,
+        labels: {
+          service: 'petclinic',
+        },
+      },
+      {
+        value: 31,
+        labels: {
+          service: 'spectest-service',
+        },
+      },
+    ],
+    aggregator: 'sum',
+  },
+  {
+    help: 'Processed spans per service',
+    name: 'sws_service_spans_total',
+    type: 'counter',
+    values: [
+      {
+        value: 1,
+        labels: {
+          service: 'hapitest',
+          kind: 'client',
+          success: true,
+        },
+      },
+      {
+        value: 94,
+        labels: {
+          service: 'petclinic',
+          kind: 'client',
+          success: true,
+        },
+      },
+      {
+        value: 25,
+        labels: {
+          service: 'spectest-service',
+          kind: 'internal',
+          success: true,
+        },
+      },
+      {
+        value: 2,
+        labels: {
+          service: 'spectest-service',
+          kind: 'server',
+          success: true,
+        },
+      },
+      {
+        value: 3,
+        labels: {
+          service: 'spectest-service',
+          kind: 'server',
+          success: false,
+        },
+      },
+      {
+        value: 3,
+        labels: {
+          service: 'petclinic',
+          kind: 'internal',
+          success: true,
+        },
+      },
+      {
+        value: 1,
+        labels: {
+          service: 'petclinic',
+          kind: 'server',
+          success: true,
+        },
+      },
+      {
+        value: 1,
+        labels: {
+          service: 'spectest-service',
+          kind: 'client',
+          success: true,
+        },
+      },
+    ],
+    aggregator: 'sum',
+  },
+  {
+    help: 'Total calls between services',
+    name: 'sws_service_calls_total',
+    type: 'counter',
+    values: [
+      {
+        value: 5,
+        labels: {
+          src: 'external',
+          dst: 'spectest-service',
+        },
+      },
+      {
+        value: 1,
+        labels: {
+          src: 'spectest-service',
+          dst: 'petclinic',
+        },
+      },
+    ],
+    aggregator: 'sum',
+  },
+];
+
 // TODO Move to test
 class Validator {
   constructor() {
@@ -31,6 +167,63 @@ class Validator {
       metricsCount: allMetrics.length,
       metrics: allMetrics,
     };
+  }
+
+  metricsToObj(allMetrics) {
+    if (!Array.isArray(allMetrics) || allMetrics.length <= 0) {
+      return {};
+    }
+    const result = {};
+    allMetrics.map((m) => {
+      const n = m.name;
+      const vo = {};
+      const vals = m.values;
+      vals.map((v) => {
+        let lkey = JSON.stringify(v.labels);
+        lkey = lkey.replaceAll('"', '');
+        vo[lkey] = v.value;
+      });
+      result[n] = vo;
+    });
+    return result;
+  }
+
+  getExpectedMetricsObj(allSpans) {
+    if (!Array.isArray(allSpans) || allSpans.length <= 0) {
+      return {};
+    }
+    const result = {
+      sws_spans_processed_total: {},
+      sws_service_calls_total: {},
+    };
+    result['sws_spans_processed_total']['{}'] = allSpans.length;
+    const spansIndex = {};
+    allSpans.map((s) => {
+      spansIndex[s.spanId] = s;
+    });
+    // Calculate sws_service_calls_total
+    // we have all spans known, so we can just analyze server spans and check if it has parent ot not
+    const labels = {};
+    allSpans.map((s) => {
+      if (s.kind === 'server') {
+        const parent = s.parentSpanId ? spansIndex[s.parentSpanId] || null : null;
+        let keySrcDst = `{src:${parent ? parent.service : 'external'},dst:${s.service}}`;
+        labels[keySrcDst] = (labels[keySrcDst] || 0) + 1;
+      }
+    });
+    result['sws_service_calls_total'] = labels;
+    return result;
+  }
+
+  async getActualMetricsObj() {
+    const allMetrics = await promClient.register.getMetricsAsJSON();
+    return this.metricsToObj(allMetrics);
+  }
+
+  async validateTest() {
+    const allMetrics = allM;
+    const metrics = this.metricsToObj(allMetrics);
+    return true;
   }
 }
 
--- packages/collector/test/processor.test.ts
+++ packages/collector/test/processor.test.ts
@@ -3,6 +3,7 @@ import matcher from '../main/matcher';
 import spanBatches from './data/spanbatches.json';
 import validator from '../main/validator';
 import { SwsSpan } from '@swaggerstats/core';
+//import { pathOr } from 'ramda';
 import { getLogger } from '@swaggerstats/core';
 const logger = getLogger('processor.test.ts');
 
@@ -16,6 +17,11 @@ jest.setTimeout(2 * 60 * 1000);
 describe('Processor Test', function () {
   it('Should initialize', async () => {
     expect((await matcher.getAll()).length === 0).toBeTruthy();
+  });
+
+  it.skip('Should test validator', async () => {
+    const vtr = await validator.validateTest();
+    expect(vtr).toBeTruthy();
   });
 
   it('Should submit span batches to processor', async () => {
@@ -32,14 +38,20 @@ describe('Processor Test', function () {
       logger.info(`Submitting ${spans.length} spans ...`);
       await processor.processSpans(spans);
     }
-    logger.info(`Wating for 15 sec ...`);
-    await pause(15000);
+    logger.info(`Wating for 5 sec ...`);
+    await pause(5000);
   });
 
   it('Should validate results', async () => {
     logger.info(`Validating results ...`);
-    const validateResult = await validator.validate();
-    expect(validateResult.success).toBeTruthy();
+    const allSpans = await matcher.getAll();
+    const expectedMetrics: any = validator.getExpectedMetricsObj(allSpans);
+    const actualMetrics: any = await validator.getActualMetricsObj();
+    //expect(pathOr(null, ['sws_spans_processed_total', '{}'], expectedMetrics)).toEqual(130);
+    expect(actualMetrics).toMatchObject(expectedMetrics);
+
+    //const validateResult = await validator.validate();
+    //expect(validateResult.success).toBeTruthy();
     //expect(true).toBeTruthy();
   });
 });
